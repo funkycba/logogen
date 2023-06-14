6fb72b113417cf105a5d713bc91244418830ec4e
Carter <carter@pangweb.com>
2023-06-13 17:19:18 -0700

    close to done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {Circle, Triangle, Square} = require('./lib/shapes.js');
 // const shapes = new SVGImageElement();
 const inquirer = require("inquirer")
-const svg = require('./svg')
+const SVG = require('./svg')
 const { writeFile } = require('fs/promises')
 // make questions, then prompt questions. Make corresponding constructors save results.(fs package)
 const questions = [{
@@ -28,7 +28,22 @@ const questions = [{
 ]
 function init() {
     inquirer.prompt(questions) // this should run the inquirer prompt.
-      .then((answers) => writeFile('logo.svg', svg.render()))
+      .then(({text, textcolor, shape, shapecolor}) => {
+        let userShape;
+        switch(shape){
+            case "circle": 
+            userShape = new Circle();
+            case "square":
+            userShape = new Square();
+            case "triangle":
+            userShape = new Triangle();
+        }
+        userShape.setColor(shapecolor);
+        const svg = new SVG();
+        svg.setText(text, textcolor);
+        svg.setShape(userShape);
+        writeFile('logo.svg', svg.render())
+      })
       .then(() => console.log('Successfully wrote'))
       .catch((err) => console.error(err));
   }
